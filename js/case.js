dce134e31513902c3c6c93186cbe60870e6b680a
Zahid-Hasan-Shuvo <shuvo.cse6.bu@gmail.com>
2023-02-15 10:53:29 +0600

    update phone price and increase and decrease

--- js/case.js
+++ js/case.js
@@ -15,16 +15,24 @@ function updateCaseNumber(isIncrease){
     return newCaseNumber;
 }
 
-
-document.getElementById('btn-case-plus').addEventListener('click', function(){
-  
- const newCaseNumber=updateCaseNumber(true);
-         const caseTotalPrice=  newCaseNumber*59;
+function updateCaseTotalPrice(newCaseNumber){
+  const caseTotalPrice=  newCaseNumber*59;
 
          const caseTotalElement=document.getElementById('case-total');
         caseTotalElement.innerText=caseTotalPrice;
-})
+
+}
+
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+ const newCaseNumber=updateCaseNumber(true);
+         updateCaseTotalPrice(newCaseNumber);
+});
+
+
 
 document.getElementById('btn-case-minus').addEventListener('click', function(){
-updateCaseNumber(false);
+  const newCaseNumber= updateCaseNumber(false);
+  updateCaseTotalPrice(newCaseNumber);
 })
